66d85aa79a898d56fb0be1ddb0c6d96693be3594
Jake <jakewarrenblack01@gmail.com>
2021-08-25 17:59:30 +0100

    Add loading icon, route from loading_screen to location_screen, add NetworkHelper and Location classes to fetch location data from device and openweatherapi

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+final apiKey = dotenv.env['OWM_KEY'];
+
+/*
+Dart null aware operator:
+- someVariable ?? defaultValue
+ */
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,15 +18,45 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    // get the user's current location
+    Location myLocation = Location();
+    await myLocation.getCurrentLocation();
+    latitude = myLocation.latitude;
+    longitude = myLocation.longitude;
+
+    // use the network helper to fetch weather info for this location
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    // will be passed to location screen
+    var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
+    // loading indicator is necessary
+    // fetching weather data takes some time
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
+        child: SpinKitDoubleBounce(
+          color: Colors.red,
+          size: 100.0,
         ),
       ),
     );
